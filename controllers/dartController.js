34121a6baf680a04a3732ab8d87381d012dee0e0
Jsegel <segeljosh@gmail.com>
2020-03-16 17:11:26 -0500

    adding structure

---
+++ controllers/dartController.js
@@ -0,0 +1,9 @@
+const express = require("express");
+var router = express.Router();
+const path = require('path');
+
+router.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 8080;
 var app = express();
+const router = express.Router();
+
+router.get('/', function(req,res){
+    res.sendFile(path.join(__dirname + '/views/index.html'))
+})
 
 app.listen(PORT, ()=>{
     console.log("Server listening on http://localhost:" + PORT)

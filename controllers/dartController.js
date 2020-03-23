d9ca42b3011292384648e1e813e30d17d38c8645
Jsegel <segeljosh@gmail.com>
2020-03-23 17:41:41 -0500

    adding 301 and cricket routes and basic page structure

--- controllers/dartController.js
+++ controllers/dartController.js
@@ -10,4 +10,12 @@ router.get('/newgame', (req,res)=>{
     res.sendFile(path.join(__dirname, '../views/newgame.html'))
 })
 
+router.get('/cricket', (req,res)=>{
+    res.sendFile(path.join(__dirname, "../views/cricket.html"))
+})
+
+router.get('/301', (req,res)=>{
+    res.sendFile(path.join(__dirname, "../views/301.html"))
+})
+
 module.exports = router;

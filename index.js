e2e02c76e93f82db8b5c37119b49f185eb5a762d
Jsegel <segeljosh@gmail.com>
2020-03-23 17:29:36 -0500

    fixing routes

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 8080;
 var app = express();
-const router = express.Router();
+var router = express.Router();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-router.get('/', function(req,res){
-    res.sendFile(path.join(__dirname + "/index.html"))
-})
+app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname + '/public')));
+
+var routes = require('./controllers/dartController');
+app.use(routes)
+
+// router.get("/", function(req,res){
+//     res.sendFile(path.join(__dirname + "/index.html"))
+// })
+
 
 app.listen(PORT, ()=>{
     console.log("Server listening on http://localhost:" + PORT)
